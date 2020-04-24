3da251f1939236ad2109dcc4b8bdb88579486408
apande74 <apande74@LHTU05CG6260GGJ.ms.ds.uhc.com>
2020-04-24 23:14:55 +0530

    Added project

---
+++ src/main/java/com/spiderinjava/demo/config/WebFluxConfig.java
@@ -0,0 +1,11 @@
+package com.spiderinjava.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebFluxConfig implements WebFluxConfigurer
+{  
+}
